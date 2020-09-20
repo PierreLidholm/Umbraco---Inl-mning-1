b7c7c0e5603fdd5ed0e319b4bb8c487d809511cc
Pierre Lidholm <pierre.lidholm@gmail.com>
2020-09-20 15:00:20 +0200

    Add project files.

---
+++ PierreLidholm.Core/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using PierreLidholm.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace PierreLidholm.Core.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        public ActionResult RenderContactForm() 
+        {
+            var vm = new ContactFormViewModel();
+            return PartialView("~/Views/Partials/Contactform.cshtml", vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleContactForm(ContactFormViewModel vm)
+        {
+            if(!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Fel", "Kolla så att allt i formen är korrekt.");
+                return CurrentUmbracoPage();
+            }
+            var contactForms = Umbraco.ContentAtRoot().DescendantsOrSelfOfType("contactForms").FirstOrDefault();
+
+            if(contactForms != null)
+            {
+                var newContact = Services.ContentService.Create("Contact", contactForms.Id, "contactForm");
+                newContact.SetValue("contactName", vm.Name);
+                newContact.SetValue("contactEmail", vm.Email);
+                newContact.SetValue("contactSubject", vm.Subject);
+                newContact.SetValue("contactComments", vm.Comnment);
+                Services.ContentService.SaveAndPublish(newContact);
+            }
+
+            TempData["status"] = "OK";
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+
+   
+}
---
+++ PierreLidholm.Core/ViewModels/ContactFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PierreLidholm.Core.ViewModels
+{
+    public class ContactFormViewModel
+    {
+        [Required]
+        [MaxLength(30, ErrorMessage = "Maxlängden är 30 tecken.")]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Ange en korrekt emailadress!")]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Maxlängden är 30 tecken.")]
+        public string Subject { get; set; }
+        [Required]
+        [MaxLength(500, ErrorMessage = "Maxlängden är 500 tecken!")]
+        public string Comnment { get; set; }
+        
+    }
+}
